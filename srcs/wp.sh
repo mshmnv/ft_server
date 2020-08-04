9cd33e421c0cddac42e966b6028bbb70cb5ff4ec
Marie Minaeva <marie@Maries-MacBook-Air.local>
2020-08-04 15:21:36 +0300

    wp.sh

--- srcs/wp.sh
+++ srcs/wp.sh
@@ -5,4 +5,4 @@ wptitle="ft_server by lbagg"
 wpemail="mshmnv@yandex.ru"
 wpuser="lbagg"
 wppass="lbagg"
-curl -d "weblog_title=$wptitle&user_name=$wpuser&admin_password=$wppass&admin_password2=$wppass&admin_email=$wpemail" -k https://$siteurl/wp-admin/install.php?step=2
+curl -d "weblog_title=$wptitle&user_name=$wpuser&admin_password=$wppass&admin_password2=$wppass&admin_email=$wpemail" -k https://$siteurl/wp-admin/install.php?step=2 > /dev/null
